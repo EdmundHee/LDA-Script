bfdf9779628f48baf4e0008452a23a6e4b01610e
edmundhee <skysblue05@gmail.com>
2016-06-16 18:01:36 +0800

    1st commit

---
+++ classifier.py
@@ -0,0 +1,84 @@
+import os, json, re, sys, numpy
+from nltk.stem import WordNetLemmatizer
+from optparse import OptionParser
+from stopword import Stopword
+from llda import LLDA
+
+class Classifier:
+
+
+    def __init__(self, options):
+
+        self.options = options
+        self.file_dir = "./build/"
+        self.stopwords = Stopword(self.file_dir).get_stopwords()
+        self.labels= []
+        self.corpus = []
+
+    def train_model(self, filename, model_name):
+        self.create_label_corpus(filename)
+        llda = LLDA(self.options.K, self.options.alpha, self.options.beta)
+        llda.set_corpus(self.labelset, self.corpus, self.labels)
+        print "M=%d, V=%d, L=%d, K=%d" % (len(self.corpus), len(llda.vocas), len(self.labelset), self.options.K)
+        for index in range(self.options.iteration):
+            sys.stderr.write("-- %d : %.4f\n" % (index, llda.perplexity()))
+        print "perplexity : %.4f" % llda.perplexity()
+        phi = llda.phi()
+        theta = llda.theta()
+        new_stopword = []
+        for k, label in enumerate(self.labelset):
+            print "\n-- label %d : %s" % (k, label)
+            # for w in numpy.argsort(-phi[k])[:30]:
+            for w in numpy.argsort(-phi[k]):
+                # if phi[k,w] >= 0.0001:
+                print "%s: %f" % (llda.vocas[w], phi[k,w])
+                if phi[k,w] <= 0.001:
+                    new_stopword.append(llda.vocas[w])
+        Stopword(self.file_dir).update_stopword(new_stopword)
+
+
+    def lemmatize(self, string):
+        return WordNetLemmatizer().lemmatize(string, pos='v')
+
+    def update_stopwords(self):
+        new_stopword = []
+        phi = llda.phi()
+        theta = llda.theta()
+        for k, label in enumerate(self.labelset):
+            for w in numpy.argsort(-phi[k]):
+                if phi[k,w] <= 0.001:
+                    new_stopword.append(llda.vocas[w])
+        Stopword(self.file_dir).update_stopword(new_stopword)
+
+    def create_label_corpus(self,filename):
+        with open(os.path.join(self.file_dir,filename)) as model:
+            for row in model:
+                label_class_list = []
+                selected_labels = []
+
+                split_row = row.lower().split("\"|\"")
+                label_array = re.sub(r'\W+',' ',split_row[0]).split()
+                # Create Unicoded label_type
+                for label_type in re.sub(r'\W+',' ',split_row[1]).split():
+                    label_class_list.append(unicode(label_type,"utf-8"))
+
+                for label in label_array[:-1]:
+                    lemmatized_label = self.lemmatize(label)
+                    if label not in self.stopwords and len(label) > 2 and not bool(re.search(r'\d',lemmatized_label)) and lemmatized_label not in self.stopwords:
+                        selected_labels.append(lemmatized_label)
+
+                self.corpus.append(selected_labels)
+                self.labels.append(label_class_list)
+                self.labelset = list(set(reduce(list.__add__, self.labels)))
+
+
+parser = OptionParser()
+parser.add_option("-f", dest="filename", type="string", help="filename", default="stopwords.txt")
+parser.add_option("--alpha", dest="alpha", type="float", help="parameter alpha", default=0.0005)
+parser.add_option("--beta", dest="beta", type="float", help="parameter beta", default=0.00125)
+parser.add_option("-k", dest="K", type="int", help="number of topics", default=50)
+parser.add_option("-i", dest="iteration", type="int", help="iteration count", default=100)
+parser.add_option("-n", dest="samplesize", type="int", help="dataset sample size", default=100)
+(options, args) = parser.parse_args()
+classifier = Classifier(options)
+classifier.train_model("test.txt","test")
---
+++ llda.py
@@ -0,0 +1,96 @@
+# Labeled Latent Dirichlet Allocation
+# This code is available under the MIT License.
+# (c)2010 Nakatani Shuyo / Cybozu Labs Inc.
+# refer to Ramage+, Labeled LDA: A supervised topic model for credit attribution in multi-labeled corpora(EMNLP2009)
+
+# SOURCE: https://github.com/shuyo/iir/blob/master/lda/llda.py
+
+import numpy
+
+class LLDA:
+
+    def __init__(self, K, alpha, beta):
+        self.K = K
+        self.alpha = alpha
+        self.beta = beta
+
+    def term_to_id(self, term):
+        if term not in self.vocas_id:
+            voca_id = len(self.vocas)
+            self.vocas_id[term] = voca_id
+            self.vocas.append(term)
+        else:
+            voca_id = self.vocas_id[term]
+        return voca_id
+
+    def complement_label(self, label):
+        if not label: return numpy.ones(len(self.labelmap))
+        vec = numpy.zeros(len(self.labelmap))
+        vec[0] = 1.0
+        for x in label: vec[self.labelmap[x]] = 1.0
+        return vec
+
+    def set_corpus(self, labelset, corpus, labels):
+        labelset.insert(0, "common")
+        self.labelmap = dict(zip(labelset, range(len(labelset))))
+        self.K = len(self.labelmap)
+        self.vocas = []
+        self.vocas_id = dict()
+        self.labels = numpy.array([self.complement_label(label) for label in labels])
+        self.docs = [[self.term_to_id(term) for term in doc] for doc in corpus]
+
+        M = len(corpus)
+        V = len(self.vocas)
+
+        self.z_m_n = []
+        self.n_m_z = numpy.zeros((M, self.K), dtype=int)
+        self.n_z_t = numpy.zeros((self.K, V), dtype=int)
+        self.n_z = numpy.zeros(self.K, dtype=int)
+
+        for m, doc, label in zip(range(M), self.docs, self.labels):
+            N_m = len(doc)
+            z_n = [numpy.random.multinomial(1, label / label.sum()).argmax() for x in range(N_m)]
+            self.z_m_n.append(z_n)
+            for t, z in zip(doc, z_n):
+                self.n_m_z[m, z] += 1
+                self.n_z_t[z, t] += 1
+                self.n_z[z] += 1
+
+
+    def inference(self):
+        V = len(self.vocas)
+        for m, doc, label in zip(range(len(self.docs)), self.docs, self.labels):
+            for n in range(len(doc)):
+                t = doc[n]
+                z = self.z_m_n[m][n]
+                self.n_m_z[m, z] -= 1
+                self.n_z_t[z, t] -= 1
+                self.n_z[z] -= 1
+
+                denom_a = self.n_m_z[m].sum() + self.K * self.alpha
+                denom_b = self.n_z_t.sum(axis=1) + V * self.beta
+                p_z = label * (self.n_z_t[:, t] + self.beta) / denom_b * (self.n_m_z[m] + self.alpha) / denom_a
+                new_z = numpy.random.multinomial(1, p_z / p_z.sum()).argmax()
+
+                self.z_m_n[m][n] = new_z
+                self.n_m_z[m, new_z] += 1
+                self.n_z_t[new_z, t] += 1
+                self.n_z[new_z] += 1
+    def phi(self):
+        V = len(self.vocas)
+        return (self.n_z_t + self.beta) / (self.n_z[:, numpy.newaxis] + V * self.beta)
+
+    def theta(self):
+        n_alpha = self.n_m_z + self.labels * self.alpha
+        return n_alpha / n_alpha.sum(axis=1)[:, numpy.newaxis]
+
+    def perplexity(self, docs=None):
+        if docs == None: docs = self.docs
+        phi = self.phi()
+        thetas = self.theta()
+        log_per = N = 0
+        for doc, theta in zip(docs, thetas):
+            for w in doc:
+                log_per -= numpy.log(numpy.inner(phi[:,w], theta))
+            N += len(doc)
+        return numpy.exp(log_per / N)
